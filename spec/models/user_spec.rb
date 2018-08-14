9981015f7928ec45d2146b0dabce84476d2fcc29
Tim Caldwell <tim.caldwell@einti.com>
2018-08-14 17:08:58 +1000

    rework user spec, remove superfluous describe and change describes to context where appropriate

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,85 +15,83 @@
 require 'rails_helper'
 
 RSpec.describe User, as: :model do
-  describe 'User' do
-    let(:user) { User.create }
+  let(:user) { User.create }
 
-    it 'has a secure password' do
-      expect(user.password_digest).to be nil
-      user.password = 'chicken'
-      expect(user.password_digest).to_not be nil
-      expect(user.authenticate 'chicken').to be user
+  it 'has a secure password' do
+    expect(user.password_digest).to be nil
+    user.password = 'chicken'
+    expect(user.password_digest).to_not be nil
+    expect(user.authenticate 'chicken').to be user
+  end
+
+  it "doesn't allow invalid data" do
+    expect(User.all.size).to eq 0
+  end
+
+  it "saves with valid data" do
+    User.create(
+      name: "Test",
+      email: "test@test.com",
+      password: "chicken",
+      username: "test"
+    )
+    expect(User.all.size).to eq 1
+  end
+
+  it "doesnt allow duplicate username or email address" do
+    User.create(
+      name: "Test",
+      email: "test@test.com",
+      password: "chicken",
+      username: "test"
+    )
+
+    User.create(
+      name: "Test2",
+      email: "test@test.com", #duplicate email
+      password: "chicken",
+      username: "test2"
+    )
+
+    User.create(
+      name: "Test3",
+      email: "test3@test.com",
+      password: "chicken",
+      username: "test" #duplicate username
+    )
+
+    expect(User.all.size).to eq 1
+  end
+
+  context 'when a customer' do
+    let(:user) { Customer.create :password => 'chicken' }
+
+    it 'is a customer user' do
+      expect(user.class).to be Customer
     end
 
-    it "doesn't allow invalid data" do
-      expect(User.all.size).to eq 0
+    it 'has single table inheritance from user' do
+      expect(user.is_a?(User)).to be true
     end
 
-    it "saves with valid data" do
-      User.create(
-        name: "Test",
-        email: "test@test.com",
-        password: "chicken",
-        username: "test"
-      )
-      expect(User.all.size).to eq 1
+    it 'remembers its type' do
+      expect(User.find user.id).to eq user
+    end
+  end
+
+  context 'when a supplier' do
+    let(:user) { Supplier.create :password => 'chicken' }
+
+    it 'is supplier user' do
+      expect(user.class).to be Supplier
     end
 
-    it "doesnt allow duplicate username or email address" do
-      User.create(
-        name: "Test",
-        email: "test@test.com",
-        password: "chicken",
-        username: "test"
-      )
-
-      User.create(
-        name: "Test2",
-        email: "test@test.com", #duplicate email
-        password: "chicken",
-        username: "test2"
-      )
-
-      User.create(
-        name: "Test3",
-        email: "test3@test.com",
-        password: "chicken",
-        username: "test" #duplicate username
-      )
-
-      expect(User.all.size).to eq 1
+    it 'has single table inheritance from user' do
+      expect(user.is_a?(User)).to be true
     end
 
-    describe 'customer' do
-      let(:user) { Customer.create :password => 'chicken' }
-
-      it 'is a customer user' do
-        expect(user.class).to be Customer
-      end
-
-      it 'has single table inheritance from user' do
-        expect(user.is_a?(User)).to be true
-      end
-
-      it 'remembers its type' do
-        expect(User.find user.id).to eq user
-      end
-    end
-
-    describe 'supplier' do
-      let(:user) { Supplier.create :password => 'chicken' }
-
-      it 'is supplier user' do
-        expect(user.class).to be Supplier
-      end
-
-      it 'has single table inheritance from user' do
-        expect(user.is_a?(User)).to be true
-      end
-
-      it 'remembers its type' do
-        expect(User.find user.id).to eq user
-      end
+    it 'remembers its type' do
+      expect(User.find user.id).to eq user
     end
   end
 end
